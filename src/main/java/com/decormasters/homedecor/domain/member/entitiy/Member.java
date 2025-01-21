eb41f4a92223b3429a55ee8e8a29411e6923f1db
futuredeveloper41 <mj10283@naver.com>
2025-01-21 11:39:37 +0900

    [Feature/feed] render user data

--- src/main/java/com/decormasters/homedecor/domain/member/entitiy/Member.java
+++ src/main/java/com/decormasters/homedecor/domain/member/entitiy/Member.java
@@ -18,6 +18,6 @@ public class Member {
     private String password;
     private String nickname;
     private LocalDateTime createdAt;
-    private String imageUrl;
+    private String imgUrl;
 
 }
