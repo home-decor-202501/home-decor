e96e7fd5a02e8fe7f86c176fb429a6290054d7d5
futuredeveloper41 <mj10283@naver.com>
2025-01-22 15:50:26 +0900

    [Feature/login] modify websuecirty config so that we can allow other computers to access to our database and server

--- src/main/java/com/decormasters/homedecor/config/auth/SecurityConfig.java
+++ src/main/java/com/decormasters/homedecor/config/auth/SecurityConfig.java
@@ -13,6 +13,13 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 // WebSeucirty에 대한 Configuration 파일이다.
 @Configuration
@@ -27,8 +34,9 @@ public class SecurityConfig {
         http
             // csrf : A 사이트에 방문했던 사용자가 스팸사이트에 방문했을 때, 스팸사이트에서 A 사이트에 사용자의 브라우저 쿠기를 가지고 악의적인 요청을 보냄
             .csrf(AbstractHttpConfigurer::disable)
-            // cors : CORS 설정은 다른 사이트에서 API를 요청할 때, 그 요청을 허용할지 말지를 결정하는 설정
-            .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configure(http))
+            .cors(withDefaults())
+            // cors : CORS 설정은 다른 사이트에서 API를 요청할 때, 그 요청을 허용할지 말지를 결정하는 설정 (프로젝트 발표를 위에 아래에 별도 설정하였음)
+//            .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configure(http))
             // 세션 인증을 비활성화
             .sessionManagement(
                     httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -52,6 +60,21 @@ public class SecurityConfig {
 
 
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // 모든 도메인 허용
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+
+
 
 
 }
